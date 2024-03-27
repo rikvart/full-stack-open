0816b7889665ef4e9db4f0850bec7adbe8b129ce
rikvart <vartiainenriku@gmail.com>
2024-03-27 20:47:16 +0200

    1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,11 @@
-import button, { useState } from "react";
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 const Button = (props) => {
   return (
-    <div>
+    <>
       <button onClick={props.handleClick}>{props.text}</button>
-    </div>
+    </>
   );
 };
 
@@ -13,24 +13,28 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  
 
   return (
     <div>
-      <h1>
-        Give feedback
-      </h1>
-      <div className="buttons">
+      <h1>Give feedback</h1>
+      <div>
         <Button handleClick={() => setGood(good + 1)} text="good" />
         <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
         <Button handleClick={() => setBad(bad + 1)} text="bad" />
       </div>
-      <h1>
-        Statistics
-      </h1>
+      <h1>Statistics</h1>
       <div className="stats">
-        Good {good}
-        Neutral {neutral}
-        Bad {bad}
+        <div>Good {good}</div>
+        <div>Neutral {neutral}</div>
+        <div>Bad {bad}</div>
+        <div>All {total}</div>
+        <div>Average</div>
+        <div>Total</div>
       </div>
     </div>
   );
