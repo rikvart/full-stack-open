c3f27d3928dbe40e5b56dc3a9ff9a7f82dfbdc11
rikvart <vartiainenriku@gmail.com>
2024-03-27 20:15:28 +0200

    part 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
+import button, { useState } from "react";
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleClick}>{props.text}</button>
+    </div>
+  );
+};
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
+      <h1>
+        Give feedback
+      </h1>
+      <div>
+        <Button handleClick={() => setGood(good + 1)} text="good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+        <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      </div>
+      <h1>
+        Statistics
+      </h1>
+      <div id="stats">
+        Good {good}
+        Neutral {neutral}
+        Bad {bad}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
