be18ea6d90c82ea5ed74065f5b2edde7ebcf8868
rikvart <vartiainenriku@gmail.com>
2024-04-18 18:01:33 +0300

    tasks 2.6-2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,53 +1,48 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-
-  const [newName, setNewName] = useState(
-    'a new note...'
-  ) 
+  const [newName, setNewName] = useState("a new note...");
 
   const addName = (event) => {
-      event.preventDefault();
-      setPersons(persons + newName)
-      console.log("event clicked", event.target)
-
-  }
-
+    event.preventDefault();
+    setPersons(persons + newName);
+    console.log("event clicked", event.target);
+  };
 
   const handleNameChange = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
 
   const Name = (props) => {
-    <>
-      {props.name}
-    </>
-  }
+    return (
+         <>{props.name}</>
+    );
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleNameChange}/>
+          name: <input onChange={handleNameChange} />
         </div>
         <div>
-          <button type="submit" onClick={addName}>add</button>
+          <button type="submit" onClick={addName}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {persons.map((person) => (
           <Name key={person.name} name={person.name} />
-        )}
+        ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
