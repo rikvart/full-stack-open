cd4617a35764258fb1ed7bf29cf79caa5f096e0e
rikvart <vartiainenriku@gmail.com>
2024-09-02 14:00:31 +0300

    work on coutry lookup

--- part2/countrydata/src/App.jsx
+++ part2/countrydata/src/App.jsx
@@ -1,32 +1,27 @@
 import { useEffect, useState } from "react";
 import Search from "./components/Search";
-import axios from 'axios';
+import axios from "axios";
 import Countrylist from "./components/Countrylist";
 
 function App() {
+  const [countryData, setCountryData] = useState([]);
+  const [searchCountry, setSearchCountry] = useState({});
 
-  const [countryData, setCountryData] = useState({});
-
-  const getAll = () => {
-    const request = axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
-    return request.then(response => response.data)
-    
-  }
-  
   useEffect(() => {
-    getAll().then((initialData) => {
-      setCountryData(initialData);
-    });
+    axios
+      .get("https://studies.cs.helsinki.fi/restcountries/api/all")
+      .then((response) => {
+        setCountryData(response.data); // Access the data property
+      });
   }, []);
-
-
-  console.log("country data", countryData)
+  
+ 
+  console.log("countrydata", countryData);
 
   return (
     <div>
       <Search />
-      <Countrylist 
-      data={countryData}/>
+      <Countrylist countryData={countryData} />
     </div>
   );
 }
--- part2/countrydata/src/components/Countrylist.jsx
+++ part2/countrydata/src/components/Countrylist.jsx
@@ -1,13 +1,25 @@
-function Countrylist(props) {
+import { useState, useEffect } from "react";
 
+function Countrylist({ countryData }) {
+  const [dataToList, setDataToList] = useState([]);
 
-    console.log("props", props)
+  // Update the state when the `countryData` prop changes
+  useEffect(() => {
+    if (Array.isArray(countryData)) {
+      setDataToList(countryData);
+    }
+  }, [countryData]);
 
+  // Ensure that dataToList is an array before rendering
   return (
     <ul>
-      {props.map((item) => (
-        <li key={item}>{item}</li>
-      ))}
+      {dataToList.length > 0 ? (
+        dataToList.map((country) => (
+          <li key={country.name.common}>{country.name.common}</li> // Assuming `name.common` is the country name
+        ))
+      ) : (
+        <p>Loading countries...</p>
+      )}
     </ul>
   );
 }
