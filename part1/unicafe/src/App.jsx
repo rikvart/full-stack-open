91093cb2738d830747fec481845b9d148e753a0e
rikvart <vartiainenriku@gmail.com>
2026-03-09 19:47:58 +0200

    task 1.8 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,35 +3,20 @@ import { useState } from 'react'
 
 const Statistics = (props) => {
 
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const total = good + neutral + bad
-  const average = ((good * 1) + (bad * -1) )/ total
-  const positive = good / total * 100 + "%"
-
-
+  const total = props.good + props.neutral + props.bad
+  const average = ((props.good * 1) + (props.bad * -1) )/ total
+  const positive = props.good / total * 100 + "%"
   return (
     <div>
-      <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
+       <h1>Statistics</h1>
       <div>
-      </div>
-      <h1>Statistics</h1>
-      <div>
-        Good = {good}
+        Good = {props.good}
       </div>
       <div>
-        Neutral = {neutral}
+        Neutral = {props.neutral}
       </div>
       <div>
-        Bad = {bad}
+        Bad = {props.bad}
       </div>
       <div>
         all {total}
@@ -43,6 +28,27 @@ const App = () => {
         {positive}
       </div>
     </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <div>
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+   </div>
     
   )
 }
